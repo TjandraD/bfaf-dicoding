e75b57dd1022ecf1a472b3415367a81128f5cd2e
TjandraD <t.darmo.t@gmail.com>
2021-01-26 11:48:33 +0700

    Finished Navigation & Routing module

--- navigation_routing/lib/main.dart
+++ navigation_routing/lib/main.dart
@@ -7,6 +7,181 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstScreen(),
+        '/secondScreen': (context) => SecondScreen(),
+        '/secondScreenWithData': (context) => SecondScreenWithData(),
+        '/returnDataScreen': (context) => ReturnDataScreen(),
+        '/replacementScreen': (context) => ReplacementScreen(),
+        '/anotherScreen': (context) => AnotherScreen(),
+      },
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation & Routing'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/secondScreen');
+              },
+              child: Text('Go to Second Screen'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/secondScreenWithData',
+                  arguments: 'Hello from First Screen!',
+                );
+              },
+              child: Text('Navigation with Data'),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                final result =
+                    await Navigator.pushNamed(context, '/returnDataScreen');
+                SnackBar snackBar = SnackBar(
+                  content: Text(result),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Text('Return Data from Another Screen'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/replacementScreen');
+              },
+              child: Text('Replace Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Back'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreenWithData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String data = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(data),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReturnDataScreen extends StatelessWidget {
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                controller: textController,
+                decoration: InputDecoration(
+                  labelText: 'Input your name',
+                ),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context, textController.text);
+              },
+              child: Text('Send'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReplacementScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/anotherScreen');
+          },
+          child: Text('Open Another Screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class AnotherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Back to First Screen'),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
