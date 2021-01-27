0f83577dd854cb2e65f5e62f2e96a44c8d32e2a8
TjandraD <t.darmo.t@gmail.com>
2021-01-27 11:55:22 +0700

    Finished News App codelab

---
+++ news_app/lib/webview_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleWebView extends StatelessWidget {
+  static const routeName = '/artile_web';
+
+  final String url;
+
+  ArticleWebView({@required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+      body: WebView(
+        initialUrl: url,
+      ),
+    );
+  }
+}
