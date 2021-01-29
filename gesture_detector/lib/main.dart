723f6d3a800b1184f62b153b4ea17fc96c31ddc4
TjandraD <t.darmo.t@gmail.com>
2021-01-29 10:21:31 +0700

    Finished module Gesture Detector

--- gesture_detector/lib/main.dart
+++ gesture_detector/lib/main.dart
@@ -7,6 +7,93 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Gesture Detector',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final double boxSize = 150.0;
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
+
+  double posX = 0.0;
+  double posY = 0.0;
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30;
+
+    setState(() {
+      this.posX = posX;
+      this.posY = posY;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (posX == 0) {
+      center(context);
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: posX,
+            top: posY,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  numTaps++;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  numDoubleTaps++;
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  numLongPress++;
+                });
+              },
+              onPanUpdate: (DragUpdateDetails details) {
+                setState(() {
+                  double deltaX = details.delta.dx;
+                  double deltaY = details.delta.dy;
+                  posX += deltaX;
+                  posY += deltaY;
+                });
+              },
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.yellow,
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          'Taps: $numTaps - Double Taps: $numDoubleTaps - Long Press: $numLongPress',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+    );
   }
 }
